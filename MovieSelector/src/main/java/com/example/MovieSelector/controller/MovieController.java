9d2c63ecb50cc51532f418d9c1967ab8f02b3517
Isabelle-1 <isabelle.lathlean@gmail.com>
2021-08-19 11:25:23 +1000

    Changed name of Controller

--- MovieSelector/src/main/java/com/example/MovieSelector/controller/MovieController.java
+++ MovieSelector/src/main/java/com/example/MovieSelector/controller/MovieController.java
@@ -2,19 +2,21 @@ package com.example.MovieSelector.controller;
 //redirects you to what you need to use, tells you where to go
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.stereotype.Controller;
 import com.example.MovieSelector.entity.Movie;
 import com.example.MovieSelector.service.MovieService;
 
-@RequestMapping("api/v1/movie")
-public class Controller {
+
+@Controller
+public class MovieController {
 	@Autowired
 	MovieService movieService;
 
-	public Controller(MovieService personService) {
+	public MovieController(MovieService personService) {
 		this.movieService = movieService;
 	}
 
